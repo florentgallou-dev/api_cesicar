779eb32bd950438f49de3528dc79e0288c7aeb18
Florent GALLOU <florentgallou@gmail.com>
2023-07-17 19:31:13 +0200

    prepaired route for register

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,22 +3,30 @@
 namespace App\Controller;
 
 use Exception;
+use App\Entity\User;
+use ApiPlatform\Api\IriConverterInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class SecurityController extends AbstractController
 {
     #[Route('/api/login', name: 'api_login', methods: ['POST'])]
-    public function index(): void
+    public function login(IriConverterInterface $iriConverter, #[CurrentUser] User $user)
     {
-        throw new Exception('Erreur d\'authentification utilisez le JWT Handler');
+        return new Response(null, 204, [
+            'Location' => $iriConverter->getIriFromResource($user),
+        ]);
+        throw new Exception('Erreur d\'authentification.');
     }
 
     #[Route('/api/register', name: 'api_register', methods: ['POST'])]
-    public function register(): void
+    public function register(Request $request): Response
     {
         dd('michel');
-        throw new Exception('Erreur d\'authentification utilisez le JWT Handler');
+        throw new Exception('Erreur d\'enregistrement');
     }
 
 }
