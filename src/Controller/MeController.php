e77c31df64875f488712ac97ccc9c1f0363a0a37
Florent GALLOU <florentgallou@gmail.com>
2023-07-27 00:15:03 +0200

    updated cors politics and api/me routes

--- src/Controller/MeController.php
+++ src/Controller/MeController.php
@@ -2,8 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\Attribute\AsController;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
 use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
 
 #[AsController]
@@ -20,5 +27,34 @@ class MeController
             throw new UnauthorizedHttpException('OAuth', 'Erreur d\'authentification');
         }
         return $user;
+    }
+
+    #[Route('/api/me', name: 'patch_user', methods: ['PATCH'])]
+    public function patchUser(#[CurrentUser] User $user, Request $request, UserRepository $userRepository, EntityManagerInterface $entityManager)
+    {
+        $REGISTRATION_REGEX = '/^[A-Z]{2}[-][0-9]{3}[-][A-Z]/';
+        $datas = json_decode($request->getContent(), true);
+
+        if(!preg_match($REGISTRATION_REGEX, $datas['car_registration'])) {
+            return new JsonResponse([
+                'error' => 'Votre immatriculation n\'est pas valide'
+            ], 410);
+        }
+
+        $user->setFirstName($datas['first_name']);
+        $user->setLastName($datas['last_name']);
+        $user->setGender($datas['gender']);
+        $user->setPosition(json_decode($datas['position'], true));
+        $user->setDriver($datas['driver']);
+        $user->setCarType($datas['car_type']);
+        $user->setCarRegistration($datas['car_registration']);
+        $user->setCarNbPlaces($datas['car_nb_places']);
+        
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        return new JsonResponse([
+            'success' => 'Vos informations on bien été enreristrées'
+        ], 200);
     }
 }
